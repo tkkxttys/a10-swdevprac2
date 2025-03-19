2086ed367eea50af12ab343b0002d6b06254435b
tkkxttys <kittycatty.2548@gmail.com>
2025-03-20 02:46:14 +0700

    edit slice

--- src/redux/features/bookSlice.ts
+++ src/redux/features/bookSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+/*import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type BookState = {
     bookItems: BookingItem[]
@@ -22,6 +22,47 @@ export const bookSlice = createSlice ({
             })
             state.bookItems = remainItems
         }
+    }
+})
+
+export const {addBooking, removeBooking} = bookSlice.actions
+export default bookSlice.reducer*/
+
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type BookState = {
+    bookItems: BookingItem[]
+}
+
+const initialState: BookState = {bookItems:[]}
+
+export const bookSlice = createSlice ({
+    name: "book",
+    initialState,
+    reducers: {
+        addBooking: (state, action: PayloadAction<BookingItem>) => {
+            const exists = state.bookItems.some(item => 
+                item.nameLastname === action.payload.nameLastname &&
+                item.tel === action.payload.tel &&
+                item.venue === action.payload.venue &&
+                item.bookDate === action.payload.bookDate
+            );
+            if (!exists) {
+                state.bookItems.push(action.payload);
+            }
+        },
+        removeBooking: (state, action: PayloadAction<BookingItem>) => {
+            const index = state.bookItems.findIndex(
+                (obj) =>
+                    obj.nameLastname === action.payload.nameLastname &&
+                    obj.tel === action.payload.tel &&
+                    obj.venue === action.payload.venue &&
+                    obj.bookDate === action.payload.bookDate
+            );
+            if (index !== -1) {
+                state.bookItems.splice(index, 1);  // ลบ item ที่ตรง
+            }
+        }        
     }
 })
 
